00c5e88b3732573d4e7436d7e550dc28f2358cf5
Kyrie Belk <kbelk@wlapcs.org>
2017-04-11 17:28:17 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,42 @@
+ var x = 10;
+ var y = 50;
+function setup(){
+    createCanvas(600,500);
+    
+}
+function draw(){
+  rect(200,y,300,200);
+        
+        fill(0,240,0);
+    y++;
+     rect(y,100,200);
+     
+     fill(0,400,300);
+     y++;
+     ellipse(y,200,200)
+   fill(0,240,0);
+    y++;
+     rect(y,100,200);
+     
+     fill(0,400,30);
+     y++;
+     ellipse(y,100,100)
+   
+   
+      fill(0,240,0);
+    y++;
+     rect(y,100,200);
+     
+     fill(0,400,300);
+     y++;
+     ellipse(y,300,200)
+   fill(0,240,0);
+    y++;
+     rect(y,100,200);
+     
+     fill(0,300,30);
+     y++;
+     ellipse(y,150,100)
+   
+}
+
